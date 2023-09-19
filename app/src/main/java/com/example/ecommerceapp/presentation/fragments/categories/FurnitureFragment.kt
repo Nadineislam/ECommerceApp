2e1aa2ddf6096f3b52962621fac7971edace9e09
Nadine Islam <nadeen.kamaal@gmail.com>
2023-09-19 18:52:08 +0300

    implement FurnitureFragment

--- app/src/main/java/com/example/ecommerceapp/presentation/fragments/categories/FurnitureFragment.kt
+++ app/src/main/java/com/example/ecommerceapp/presentation/fragments/categories/FurnitureFragment.kt
@@ -1,4 +1,67 @@
 package com.example.ecommerceapp.presentation.fragments.categories
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.example.ecommerceapp.data.Category
+import com.example.ecommerceapp.data.repository.ShoppingRepository
+import com.example.ecommerceapp.presentation.viewmodel.CategoryViewModel
+import com.example.ecommerceapp.presentation.viewmodel.CategoryViewModelFactory
+import com.example.ecommerceapp.utils.Resource
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.firestore.FirebaseFirestore
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@AndroidEntryPoint
 class FurnitureFragment:BaseCategoryFragment() {
+    @Inject
+    lateinit var fireStore: FirebaseFirestore
+    private val shoppingRepository: ShoppingRepository by lazy { ShoppingRepository(fireStore) }
+    private val viewModel by viewModels<CategoryViewModel> {
+        CategoryViewModelFactory(fireStore, Category.Furniture, shoppingRepository)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        lifecycleScope.launch {
+            viewModel.offerProducts.collectLatest {
+                when (it) {
+                    is Resource.Loading -> {}
+                    is Resource.Success -> {
+                        offerAdapter.differList.submitList(it.data)
+                    }
+
+                    is Resource.Error -> {
+                        Snackbar.make(requireView(), it.message.toString(), Snackbar.LENGTH_LONG)
+                            .show()
+                    }
+
+                    else -> Unit
+                }
+
+            }
+        }
+        lifecycleScope.launch {
+            viewModel.bestProducts.collectLatest {
+                when (it) {
+                    is Resource.Loading -> {}
+                    is Resource.Success -> {
+                        bestProductsAdapter.differList.submitList(it.data)
+                    }
+
+                    is Resource.Error -> {
+                        Snackbar.make(requireView(), it.message.toString(), Snackbar.LENGTH_LONG)
+                            .show()
+                    }
+
+                    else -> Unit
+                }
+
+            }
+        }
+    }
 }
