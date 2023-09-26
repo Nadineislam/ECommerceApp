ad180de08589dbce9c52f3025155e925ee20fa1b
Nadine Islam <nadeen.kamaal@gmail.com>
2023-09-26 10:43:31 +0300

    Update ShoppingRepository

--- app/src/main/java/com/example/ecommerceapp/data/repository/ShoppingRepository.kt
+++ app/src/main/java/com/example/ecommerceapp/data/repository/ShoppingRepository.kt
@@ -4,8 +4,9 @@ import com.example.ecommerceapp.data.Product
 import com.example.ecommerceapp.utils.Resource
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.tasks.await
+import javax.inject.Inject
 
-class ShoppingRepository(private val fireStore: FirebaseFirestore) {
+class ShoppingRepository @Inject constructor(private val fireStore: FirebaseFirestore) {
     suspend fun fetchOfferProducts(category: String): Resource<List<Product>> {
         return try {
             val querySnapshot = fireStore.collection("Products")
@@ -18,6 +19,7 @@ class ShoppingRepository(private val fireStore: FirebaseFirestore) {
             Resource.Error(e.message.toString())
         }
     }
+
     suspend fun fetchBestProducts(category: String): Resource<List<Product>> {
         return try {
             val querySnapshot = fireStore.collection("Products")
@@ -31,4 +33,33 @@ class ShoppingRepository(private val fireStore: FirebaseFirestore) {
             Resource.Error(e.message.toString())
         }
     }
+
+    suspend fun getBestProducts(): List<Product> {
+        val pagingInfo = PagingInfo()
+        val productsCollection = fireStore.collection("Products")
+        val querySnapshot = productsCollection.limit(pagingInfo.bestProductsPage * 9).get().await()
+        val bestProductsList = querySnapshot.toObjects(Product::class.java)
+        pagingInfo.isPagingEnd = bestProductsList == pagingInfo.oldBesProducts
+        pagingInfo.oldBesProducts = bestProductsList
+        pagingInfo.bestProductsPage++
+        return bestProductsList
+    }
+
+    suspend fun getBestDealsProducts(): List<Product> {
+        val productsCollection = fireStore.collection("Products")
+        val querySnapshot = productsCollection.whereEqualTo("category", "Collections").get().await()
+        return querySnapshot.toObjects(Product::class.java)
+    }
+
+    suspend fun getSpecialProducts(): List<Product> {
+        val productsCollection = fireStore.collection("Products")
+        val querySnapshot = productsCollection.whereEqualTo("category", "Chair").get().await()
+        return querySnapshot.toObjects(Product::class.java)
+    }
+
+    internal data class PagingInfo(
+        var bestProductsPage: Long = 1,
+        var oldBesProducts: List<Product> = emptyList(),
+        var isPagingEnd: Boolean = false
+    )
 }
--- app/src/main/java/com/example/ecommerceapp/presentation/viewmodel/MainCategoryViewModel.kt
+++ app/src/main/java/com/example/ecommerceapp/presentation/viewmodel/MainCategoryViewModel.kt
@@ -3,8 +3,8 @@ package com.example.ecommerceapp.presentation.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.ecommerceapp.data.Product
+import com.example.ecommerceapp.data.repository.ShoppingRepository
 import com.example.ecommerceapp.utils.Resource
-import com.google.firebase.firestore.FirebaseFirestore
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -12,7 +12,7 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class MainCategoryViewModel @Inject constructor(private val fireStore: FirebaseFirestore) :
+class MainCategoryViewModel @Inject constructor(private val shoppingRepository: ShoppingRepository) :
     ViewModel() {
     private val _specialProducts = MutableStateFlow<Resource<List<Product>>>(Resource.Unspecified())
     val specialProducts: StateFlow<Resource<List<Product>>> = _specialProducts
@@ -23,7 +23,6 @@ class MainCategoryViewModel @Inject constructor(private val fireStore: FirebaseF
 
     private val _bestProducts = MutableStateFlow<Resource<List<Product>>>(Resource.Unspecified())
     val bestProducts: StateFlow<Resource<List<Product>>> = _bestProducts
-    private val pagingInfo = PagingInfo()
 
     init {
         getSpecialProducts()
@@ -32,64 +31,38 @@ class MainCategoryViewModel @Inject constructor(private val fireStore: FirebaseF
     }
 
     fun getBestProducts() {
-        if (!pagingInfo.isPagingEnd) {
-            viewModelScope.launch {
-                _bestProducts.emit(Resource.Loading())
+        viewModelScope.launch {
+            _bestProducts.value = Resource.Loading()
+            try {
+                val bestProductsList = shoppingRepository.getBestProducts()
+                _bestProducts.value = Resource.Success(bestProductsList)
+            } catch (e: Exception) {
+                _bestProducts.value = Resource.Error(e.message.toString())
             }
-            fireStore.collection("Products").limit(pagingInfo.bestProductsPage * 9)
-                .get().addOnSuccessListener { result ->
-                    val bestProductsList = result.toObjects(Product::class.java)
-                    pagingInfo.isPagingEnd = bestProductsList == pagingInfo.oldBesProducts
-                    pagingInfo.oldBesProducts = bestProductsList
-                    viewModelScope.launch {
-                        _bestProducts.emit(Resource.Success(bestProductsList))
-                    }
-                    pagingInfo.bestProductsPage++
-                }.addOnFailureListener {
-                    viewModelScope.launch {
-                        _bestProducts.emit(Resource.Error(it.message.toString()))
-                    }
-                }
         }
     }
 
     private fun getBestDealsProducts() {
         viewModelScope.launch {
-            _bestDealsProducts.emit(Resource.Loading())
-        }
-        fireStore.collection("Products").whereEqualTo("category", "Collections").get()
-            .addOnSuccessListener { result ->
-                val bestDealsProductsList = result.toObjects(Product::class.java)
-                viewModelScope.launch {
-                    _bestDealsProducts.emit(Resource.Success(bestDealsProductsList))
-                }
-            }.addOnFailureListener {
-            viewModelScope.launch {
-                _bestDealsProducts.emit(Resource.Error(it.message.toString()))
+            _bestDealsProducts.value = Resource.Loading()
+            try {
+                val bestDealsProductsList = shoppingRepository.getBestDealsProducts()
+                _bestDealsProducts.value = Resource.Success(bestDealsProductsList)
+            } catch (e: Exception) {
+                _bestDealsProducts.value = Resource.Error(e.message.toString())
             }
         }
     }
 
     private fun getSpecialProducts() {
         viewModelScope.launch {
-            _specialProducts.emit(Resource.Loading())
-        }
-        fireStore.collection("Products").whereEqualTo("category", "Chair").get()
-            .addOnSuccessListener { result ->
-                val specialProductsList = result.toObjects(Product::class.java)
-                viewModelScope.launch {
-                    _specialProducts.emit(Resource.Success(specialProductsList))
-                }
-            }.addOnFailureListener {
-            viewModelScope.launch {
-                _specialProducts.emit(Resource.Error(it.message.toString()))
+            _specialProducts.value = Resource.Loading()
+            try {
+                val specialProductsList = shoppingRepository.getSpecialProducts()
+                _specialProducts.value = Resource.Success(specialProductsList)
+            } catch (e: Exception) {
+                _specialProducts.value = Resource.Error(e.message.toString())
             }
         }
     }
-
-    internal data class PagingInfo(
-        var bestProductsPage: Long = 1,
-        var oldBesProducts: List<Product> = emptyList(),
-        var isPagingEnd: Boolean = false
-    )
 }
